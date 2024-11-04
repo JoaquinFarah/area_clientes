f5588f7de049381032caeac0bd80527bb2dbeb12
JFarah <joaquinefarah@gmail.com>
2024-11-04 17:49:02 -0300

    route fixes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import '../styles/globals.css';
-import { ThemeProvider } from './context/ThemeContext';
+import { ThemeProvider } from '@/context/ThemeContext'
 import Sidebar from './components/Sidebar';
 
 
