5f2e256a8222afa1648f6f89fe5a7de9db7f3f85
JFarah <joaquinefarah@gmail.com>
2024-11-04 18:06:19 -0300

    dark theme aply

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,8 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
 import { ThemeProvider } from '@/context/ThemeContext'
 import Sidebar from './components/Sidebar';
 
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
 export const metadata: Metadata = {
   title: "Area Clientes",
@@ -27,14 +15,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+    <html lang="en">
+      <body>
         <ThemeProvider>
-          <div className="flex">
+          <div className="flex dark:bg-gray-900 text-gray-900 dark:text-gray-100">
             <Sidebar />
-            <main className="p-10 flex-1">{children}</main>
+            <main className="p-10 flex-1">
+              {children}
+            </main>
           </div>
         </ThemeProvider>
       </body>
