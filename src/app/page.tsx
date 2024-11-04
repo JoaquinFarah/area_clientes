cf73e8a9e3f5e727f01deb48452bf0d84886ead2
JFarah <joaquinefarah@gmail.com>
2024-11-04 17:46:04 -0300

    main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
-import React from 'react'
-
-export default function page() {
+export default function Home() {
   return (
-    <div>INDEX PAGE</div>
-  )
+    <div>
+      <h1 className="text-2xl font-bold">Bienvenido a tu aplicación Next.js 15</h1>
+      <p>Aquí puedes agregar el contenido principal de tu aplicación.</p>
+    </div>
+  );
 }
