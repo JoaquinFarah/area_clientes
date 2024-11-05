199d157ccaba504e7c91143b507399a93cbb5cca
JFarah <joaquinefarah@gmail.com>
2024-11-05 20:03:06 -0300

    main page advance

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 export default function Home() {
   return (
-    <div>
+    <div className="mx-auto mt-10 grid max-w-lg grid-cols-4 items-center justify-between gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-2">
       <h1 className="text-2xl font-bold">DASHBOARD</h1>
-      <p>Aquí puedes agregar el contenido principal de tu aplicación.</p>
+      <p>Saldo a Favor: $0.00</p>
+      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Agregar saldo</button>
+      
     </div>
   );
 }
