03d12ea86f7604128d6c30662011a5abdb3c8c25
JFarah <joaquinefarah@gmail.com>
2024-11-05 11:15:38 -0300

    Sidebar links function add

--- src/app/components/Sidebar.tsx
+++ src/app/components/Sidebar.tsx
@@ -1,18 +1,15 @@
-"use client"
+"use client";
 import { useState } from 'react';
 import { useTheme } from '@/context/ThemeContext';
 import Image from 'next/image';
-
+import Link from 'next/link';
 
 const Sidebar = () => {
-  const [isCollapsed, setIsCollapsed] = useState(false);
   const { theme, toggleTheme } = useTheme();
 
   return (
-    <div className={`flex flex-col ${isCollapsed ? 'w-20' : 'w-64'} bg-gray-100 dark:bg-gray-800 h-screen p-5 transition-width duration-300`}>
-      {/* <button onClick={() => setIsCollapsed(!isCollapsed)} className="mb-4">
-        {isCollapsed ? '>' : '<'}
-      </button> */}
+    <div className="flex flex-col w-64 bg-gray-100 dark:bg-gray-800 h-screen p-5">
+      {/* Botón para alternar el tema */}
       <button
         onClick={toggleTheme}
         className="bg-gray-200 dark:bg-gray-700 p-2 rounded-full text-gray-600 dark:text-gray-300"
@@ -20,6 +17,7 @@ const Sidebar = () => {
         {theme === 'light' ? '🌞' : '🌜'}
       </button>
 
+      {/* Imagen superior */}
       <div className="relative h-36 w-36 mb-10 mx-auto mt-10">
         <Image
           src={theme === 'light' ? '/logo-astronauta-light.png' : '/logo-astronauta-dark.png'}
@@ -29,47 +27,60 @@ const Sidebar = () => {
         />
       </div>
 
+      {/* Menú de Navegación */}
       <nav>
-        <SidebarItem title="Item 1" />
-        <SidebarItem title="Item 2" subItems={['Subitem 2.1', 'Subitem 2.2']} />
-        <SidebarItem title="Item 3" subItems={['Subitem 3.1', 'Subitem 3.2', 'Subitem 3.3']} />
+        <SidebarItem title="Pagina Principal" href="/" />
+        <SidebarItem
+          title="Redes"
+          subItems={[
+            { label: 'Facebook', href: 'https://www.facebook.com/' },
+            { label: 'Gmail', href: 'https://mail.google.com/mail/u/0/#inbox' },
+          ]}
+        />
+        <SidebarItem
+          title="Profesional"
+          subItems={[
+            { label: 'Linkedin', href: 'https://www.linkedin.com/feed/' },
+            { label: 'YouTube', href: 'https://www.youtube.com/' },            
+          ]}
+        />
       </nav>
-
-      {/* <button
-        onClick={toggleTheme}
-        className="mt-auto bg-gray-200 dark:bg-gray-700 p-2 rounded-full text-gray-600 dark:text-gray-300"
-      >
-        {theme === 'light' ? '🌞' : '🌜'}
-      </button> */}
     </div>
   );
 };
 
-const SidebarItem = ({ title, subItems }: { title: string; subItems?: string[] }) => {
+type SidebarItemProps = {
+  title: string;
+  href?: string;
+  subItems?: { label: string; href: string }[];
+};
+
+const SidebarItem = ({ title, href, subItems }: SidebarItemProps) => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
     <div className="w-full">
-      <button
-        onClick={() => setIsOpen(!isOpen)}
-        className="flex items-center justify-between w-full p-2 text-gray-700 dark:text-gray-300 hover:bg-gray-200 dark:hover:bg-gray-600 rounded"
-      >
-        {title}
-        {subItems && (
-          <div className={`transition-transform ${isOpen ? 'rotate-180' : ''}`}>
-            <Image src="/LineMdChevronDownSquareTwotone.svg" alt="Dropdown Icon" width={20} height={20} />
-          </div>
-        )}
-      </button>
+      <Link href={href || '#'} passHref>
+        <button
+          onClick={() => setIsOpen(!isOpen)}
+          className="flex items-center justify-between w-full p-2 text-gray-700 dark:text-gray-300 hover:bg-gray-200 dark:hover:bg-gray-600 rounded"
+        >
+          {title}
+          {subItems && (
+            <div className={`transition-transform ${isOpen ? 'rotate-180' : ''}`}>
+              <Image src="/LineMdChevronDownSquareTwotone.svg" alt="Dropdown Icon" width={20} height={20} />
+            </div>
+          )}
+        </button>
+      </Link>
       {isOpen && subItems && (
         <div className="pl-4">
           {subItems.map((subItem) => (
-            <p
-              key={subItem}
-              className="p-2 text-sm text-gray-500 dark:text-gray-400 hover:bg-gray-200 dark:hover:bg-gray-600 rounded"
-            >
-              {subItem}
-            </p>
+            <Link key={subItem.href} href={subItem.href} passHref>
+              <div className="block p-2 text-sm text-gray-500 dark:text-gray-400 hover:bg-gray-200 dark:hover:bg-gray-600 rounded">
+                {subItem.label}
+              </div>
+            </Link>
           ))}
         </div>
       )}
