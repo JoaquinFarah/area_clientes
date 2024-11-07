dc5795e8211dc42b8822d40d57ded6e1a29065f4
JFarah <joaquinefarah@gmail.com>
2024-11-07 11:19:22 -0300

    fixes

--- src/app/components/ClientLevel.tsx
+++ src/app/components/ClientLevel.tsx
@@ -17,7 +17,7 @@ const ClientLevel = () => {
     if (meters >= 500) setLevel('Núcleo Fusión');
     else if (meters >= 100) setLevel('Alquimista');
     else if (meters >= 20) setLevel('Maestro Creativo');
-    else if (meters >= 5) setLevel('Explorador');
+    else if (meters >= 5) setLevel('Explorador');    
     else setLevel('Neo Iniciado');
   };
 
@@ -45,9 +45,10 @@ const ClientLevel = () => {
         <button onClick={() => addMeters(5)} className="text-gray-900 bg-gradient-to-r from-lime-200 via-lime-400 to-lime-500 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-lime-300 dark:focus:ring-lime-800 shadow-lg shadow-lime-500/50 dark:shadow-lg dark:shadow-lime-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 pr-3">Agregar 5 metros</button>
         <button onClick={() => addMeters(10)} className="text-gray-900 bg-gradient-to-r from-lime-200 via-lime-400 to-lime-500 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-lime-300 dark:focus:ring-lime-800 shadow-lg shadow-lime-500/50 dark:shadow-lg dark:shadow-lime-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 pr-3">Agregar 10 metros</button>
         <button onClick={() => addMeters(20)} className="text-gray-900 bg-gradient-to-r from-lime-200 via-lime-400 to-lime-500 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-lime-300 dark:focus:ring-lime-800 shadow-lg shadow-lime-500/50 dark:shadow-lg dark:shadow-lime-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 pr-3">Agregar 20 metros</button>
+        <button onClick={() => {setMeters(0); setImageSrc('/nivel-1.png')}} className="text-gray-900 bg-gradient-to-r from-lime-200 via-lime-400 to-lime-500 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-lime-300 dark:focus:ring-lime-800 shadow-lg shadow-lime-500/50 dark:shadow-lg dark:shadow-lime-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 pr-3">REINICIAR</button>
         <div className="mt-4">
-          <p>Metros de ejemplo cargados: {meters}</p>
-          <p>Nivel de ejemplo: {level}</p>
+          <p>Metros cargados: {meters}</p>
+          <p>Nivel: {level}</p>
         </div>
       </div>
     </div>
