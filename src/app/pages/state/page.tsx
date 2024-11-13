a94ff05d98ae1ad2307101fa4c3221a083c0c7de
JFarah <joaquinefarah@gmail.com>
2024-11-13 12:25:34 -0300

    minor fixes

--- src/app/pages/state/page.tsx
+++ src/app/pages/state/page.tsx
@@ -27,7 +27,7 @@
 //   };
 
 //   return (
-//     <div className="p-4 h-screen">
+//     <div className="p-4 h-screen w-full">
 //       <h1 className="text-2xl font-bold mb-6 text-center">Estado de los archivos de cada trabajo</h1>
 
 //       <input type="hidden" value={clienteId} />
@@ -99,9 +99,9 @@
 
 
 "use client"
-
 import { useState } from 'react';
 import { useTheme } from '@/context/ThemeContext';
+import { MdOutlineSpeakerNotesOff } from "react-icons/md";
 
 interface Trabajo {
   idError: string;
@@ -129,7 +129,7 @@ const EstadoArchivos: React.FC<EstadoArchivosProps> = ({ results, clienteId, nom
   };
 
   return (
-    <div className="p-4">
+    <div className="p-4 h-screen w-full">
       <h1 className={`text-2xl font-bold mb-6 text-center ${theme === 'dark' ? 'text-white' : 'text-gray-900'}`}>
         Estado de los archivos de cada trabajo
       </h1>
@@ -147,12 +147,13 @@ const EstadoArchivos: React.FC<EstadoArchivosProps> = ({ results, clienteId, nom
         <div className={`${theme === 'dark' ? 'bg-[#1c1b29]' : 'bg-white'} p-4 rounded-lg`}>
           <div className="flex items-center mb-4">
             <i className={`bi bi-tags-fill text-lg mr-2 ${theme === 'dark' ? 'text-teal-400' : 'text-pink-500'}`} />
-            <h5 className={`text-lg font-semibold ${theme === 'dark' ? 'text-white' : 'text-gray-900'}`}>Trabajos con errores</h5>
+            <MdOutlineSpeakerNotesOff />
+            <h5 className="ml-2 {`text-lg font-semibold ${theme === 'dark' ? 'text-white' : 'text-gray-900'}`}">Trabajos con errores</h5>
           </div>
           <div className="overflow-x-auto">
             <table className={`min-w-full border rounded-lg ${theme === 'dark' ? 'border-[#00ffa2]' : 'border-gray-300'}`}>
               <thead>
-                <tr className={`text-center ${theme === 'dark' ? 'text-white' : 'text-gray-900'}`}>
+                <tr className="{`text-center ${theme === 'dark' ? 'text-white' : 'text-gray-900'}`} shadow-lg shadow-purple-500/50 dark:shadow-lg dark:shadow-purple-800/80">
                   <th className={`px-4 py-2 border-b ${theme === 'dark' ? 'border-[#00ffa2]' : 'border-gray-300'}`}>Fecha Creación</th>
                   <th className={`px-4 py-2 border-b ${theme === 'dark' ? 'border-[#00ffa2]' : 'border-gray-300'}`}>Trabajo</th>
                   <th className={`px-4 py-2 border-b ${theme === 'dark' ? 'border-[#00ffa2]' : 'border-gray-300'}`}>Mensaje</th>
