54278d8f5716b53b2f954ce2e303bb1e2d1f44d2
JFarah <joaquinefarah@gmail.com>
2024-11-26 10:56:50 -0300

    more css

--- src/app/pages/request/workImage.tsx
+++ src/app/pages/request/workImage.tsx
@@ -68,7 +68,11 @@ export const WorkImage: React.FC = () => {
           />
           <label
             htmlFor="file-input"
-            className="cursor-pointer text-white bg-teal-500 hover:bg-teal-700 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+            className={`text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 inline-flex items-center ${
+              theme === "dark"
+                ? "dark:bg-teal-500 dark:hover:bg-teal-700 dark:focus:ring-teal-800"
+                : ""
+            }`}
           >
             Subir Archivos
           </label>
